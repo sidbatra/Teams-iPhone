83a7f71688acd6f47131b66e6f92c1a25f42d200
Siddharth Batra <siddharthabatra@gmail.com>
2011-07-06 19:17:10 -0700

    missing Team model added

---
+++ Teams/Classes/DWTeam.h
@@ -0,0 +1,65 @@
+//
+//  DWTeam.h
+//  Copyright 2011 Denwen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "DWPoolObject.h"
+
+@class DWAttachment;
+
+/**
+ * Team model representing a group of user's who work together
+ */
+@interface DWTeam : DWPoolObject {
+	NSString		*_name;
+    NSString		*_byline;
+	
+    NSInteger		_followersCount;
+    NSInteger		_membersCount;
+    
+    NSTimeInterval	_createdAtTimestamp;
+    
+	DWAttachment	*_attachment;
+}
+
+/**
+ * Name of the team
+ */
+@property (nonatomic,copy) NSString *name;
+
+/**
+ * Byline of the team
+ */
+@property (nonatomic,copy) NSString *byline;
+
+/**
+ * Total users following the team
+ */
+@property (nonatomic,readonly) NSInteger followingsCount;
+
+/**
+ * Total members of the team
+ */ 
+@property (nonatomic,readonly) NSInteger membersCount;
+
+/**
+ * Timestamp of the creation of the team
+ */
+@property (nonatomic,readonly) NSTimeInterval createdAtTimestamp;
+
+/**
+ * Optional attachment of the last item posted by the team
+ */
+@property (nonatomic,retain) DWAttachment *attachment;
+
+ 
+/**
+ * Start downloading attachment slice images
+ */
+- (void)startPreviewDownload;
+
+@end
+
+
---
+++ Teams/Classes/DWTeam.m
@@ -0,0 +1,106 @@
+//
+//  DWTeam.m
+//  Copyright 2011 Denwen. All rights reserved.
+//
+
+#import "DWTeam.h"
+#import "DWAttachment.h"
+#import "DWRequestsManager.h"
+#import "DWConstants.h"
+
+
+
+//----------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+@implementation DWTeam
+
+@synthesize name                = _name;
+@synthesize byline              = _byline;
+@synthesize followingsCount     = _followersCount;
+@synthesize membersCount        = _membersCount;
+@synthesize createdAtTimestamp  = _createdAtTimestamp;
+@synthesize attachment          = _attachment;
+
+//----------------------------------------------------------------------------------------------------
+- (id)init {
+	self = [super init];
+	
+	if(self) {
+	}
+	
+	return self;  
+}
+
+//----------------------------------------------------------------------------------------------------
+- (void)freeMemory {
+}
+
+//----------------------------------------------------------------------------------------------------
+- (void)freeAttachment {
+    [self.attachment destroy];
+    self.attachment = nil;
+}
+
+//----------------------------------------------------------------------------------------------------
+-(void)dealloc{
+	
+	NSLog(@"team released %d",self.databaseID);
+	
+	self.name			= nil;
+    self.byline         = nil;
+    
+    [self freeAttachment];
+	
+	[super dealloc];
+}
+
+//----------------------------------------------------------------------------------------------------
+- (void)update:(NSDictionary*)team {
+    [super update:team];
+
+    NSString *name                  = [team objectForKey:kKeyName];
+    NSString *byline                = [team objectForKey:kKeyByLine];
+    NSString *timestamp             = [team objectForKey:kKeyTimestamp];
+    NSString *followingsCount       = [team objectForKey:kKeyFollowingsCount];
+    NSString *membershipsCount      = [team objectForKey:kKeyMembershipsCount];    
+    NSDictionary *attachment        = [team objectForKey:kKeyAttachment];
+     
+    
+    if(name && ![self.name isEqualToString:name])
+        self.name = name;
+    
+    if(byline && ![self.byline isEqualToString:byline])
+       self.byline = byline;
+       
+    if(timestamp)
+        _createdAtTimestamp = [timestamp floatValue];
+    
+    if(followingsCount)
+        _followersCount = [followingsCount integerValue];
+    
+    if(membershipsCount)
+        _membersCount = [membershipsCount integerValue];
+    
+    if(attachment) {
+
+        if(self.attachment) {
+            
+            if([[attachment objectForKey:kKeyID] integerValue] != self.attachment.databaseID) {
+                [self freeAttachment];
+                self.attachment = [DWAttachment create:attachment];
+            }
+        }
+        else
+            self.attachment = [DWAttachment create:attachment];
+    }
+    
+}
+
+//----------------------------------------------------------------------------------------------------
+- (void)startPreviewDownload {
+	if(self.attachment)
+		[self.attachment startSliceDownload];
+}
+
+@end
