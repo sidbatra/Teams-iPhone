23db5cd2efa14434c1800371e9d5ee7c1a4e6f7e
Siddharth Batra <siddharthabatra@gmail.com>
2011-07-15 10:55:37 -0700

    adding teams container view

---
+++ Teams/Classes/Containers/DWTeamsContainerViewController.h
@@ -0,0 +1,17 @@
+//
+//  DWTeamsContainerViewController.h
+//  Copyright 2011 Denwen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "DWContainerViewController.h"
+
+/**
+ * Root view for the teams navigation controller
+ */
+@interface DWTeamsContainerViewController : DWContainerViewController {
+    
+}
+
+@end
