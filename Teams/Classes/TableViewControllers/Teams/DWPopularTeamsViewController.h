691091b09d7183005f61c97019742f8d640912eb
Siddharth Batra <siddharthabatra@gmail.com>
2011-07-15 16:32:34 -0700

    popular teams data source created

---
+++ Teams/Classes/TableViewControllers/Teams/DWPopularTeamsViewController.h
@@ -0,0 +1,24 @@
+//
+//  DWPopularTeamsViewController.h
+//  Copyright 2011 Denwen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "DWTeamsViewController.h"
+
+@class DWPopularTeamsDataSource;
+
+/**
+ * Table view for displaying popular teams
+ */
+@interface DWPopularTeamsViewController : DWTeamsViewController {
+    DWPopularTeamsDataSource    *_popularTeamsDataSource;   
+}
+
+/**
+ * Data source for the table view
+ */
+@property (nonatomic,retain) DWPopularTeamsDataSource *popularTeamsDataSource;
+
+@end
---
+++ Teams/Classes/TableViewControllers/Teams/DWPopularTeamsViewController.m
@@ -0,0 +1,49 @@
+//
+//  DWPopularTeamsViewController.m
+//  Copyright 2011 Denwen. All rights reserved.
+//
+
+#import "DWPopularTeamsViewController.h"
+#import "DWPopularTeamsDataSource.h"
+
+
+
+//----------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+@implementation DWPopularTeamsViewController
+
+@synthesize popularTeamsDataSource  = _popularTeamsDataSource;
+
+//----------------------------------------------------------------------------------------------------
+- (id)init {
+    self = [super init];
+    
+    if(self) {
+        self.popularTeamsDataSource = [[[DWPopularTeamsDataSource alloc] init] autorelease];
+    }
+    
+    return self;
+}
+
+//----------------------------------------------------------------------------------------------------
+- (void)dealloc {    
+    self.popularTeamsDataSource  = nil;
+    
+    [super dealloc];
+}
+
+//----------------------------------------------------------------------------------------------------
+- (DWTableViewDataSource*)getDataSource {
+    return self.popularTeamsDataSource;
+}
+
+//----------------------------------------------------------------------------------------------------
+- (void)viewDidLoad {
+	[super viewDidLoad];
+    
+    [self.popularTeamsDataSource loadTeams];
+}
+
+
+@end
