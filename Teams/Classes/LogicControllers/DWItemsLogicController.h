5bf864af067c1231427e296bed7b1297e4766ee0
Siddharth Batra <siddharthabatra@gmail.com>
2011-07-16 17:33:18 -0700

    ItemsViewController moved to the pattern of ItemsLogicController

---
+++ Teams/Classes/LogicControllers/DWItemsLogicController.h
@@ -0,0 +1,69 @@
+//
+//  DWItemsLogicController.h
+//  Copyright 2011 Denwen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "DWItemsDataSource.h"
+#import "DWTouchesController.h"
+
+@class DWItem;
+@class DWTeam;
+@class DWUser;
+@class DWTouchesController;
+@class DWTableViewController;
+@protocol DWItemsLogicControllerDelegate;
+
+/**
+ * Base class for table views that display a list of items
+ */
+@interface DWItemsLogicController : NSObject<DWTouchesControllerDelegate> {
+    DWTouchesController     *_touchesController;
+    DWTableViewController   *_tableViewController;
+
+    
+    id<DWItemsLogicControllerDelegate,NSObject> _delegate;
+}
+
+/**
+ * The table view controller which contains the items view controller object
+ */
+@property (nonatomic,assign) DWTableViewController *tableViewController;
+
+/**
+ * Record the touches made on items by the current user
+ */
+@property (nonatomic,retain) DWTouchesController *touchesController;
+
+/**
+ * Delegate fires events about the ItemsLogicController lifecycle
+ */
+@property (nonatomic,assign) id<DWItemsLogicControllerDelegate,NSObject> delegate;
+
+@end
+
+
+/**
+ * Delegate protocol to receive events about the ItemsLogicController lifecycle
+ */
+@protocol DWItemsLogicControllerDelegate
+
+@optional
+
+/**
+ * Fired when a team is selected
+ */
+- (void)itemsLogicTeamSelected:(DWTeam*)team;
+
+/**
+ * Fired when a user is selected
+ */
+- (void)itemsLogicUserSelected:(DWUser*)user;
+
+/**
+ * Fired when the share button or an item is selected
+ */
+- (void)itemsLogicShareSelectedForItem:(DWItem*)item;
+
+@end
