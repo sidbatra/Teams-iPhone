c7a1eec164eea4edec9947799977cad93ce58aa4
Siddharth Batra <siddharthabatra@gmail.com>
2011-07-17 23:22:53 -0700

    user presenter created

---
+++ Teams/Classes/ModelPresenters/DWUserPresenter.h
@@ -0,0 +1,17 @@
+//
+//  DWUserPresenter.h
+//  Copyright 2011 Denwen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "DWModelPresenter.h"
+
+/**
+ * Presenter for the user model
+ */
+@interface DWUserPresenter : NSObject<DWModelPresenter> {
+    
+}
+
+@end
---
+++ Teams/Classes/ModelPresenters/DWUserPresenter.m
@@ -0,0 +1,89 @@
+//
+//  DWUserPresenter.m
+//  Copyright 2011 Denwen. All rights reserved.
+//
+
+#import "DWUserPresenter.h"
+#import "DWUser.h"
+#import "DWSlimCell.h"
+#import "DWConstants.h"
+
+static CGFloat const kCellHeight  = 60;
+
+
+
+//----------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+@implementation DWUserPresenter
+
+//----------------------------------------------------------------------------------------------------
++ (UITableViewCell*)cellForObject:(id)object
+                     withBaseCell:(id)base
+               withCellIdentifier:(NSString*)identifier
+                     withDelegate:(id)delegate
+             andPresentationStyle:(NSInteger)style {
+    
+    DWUser *user        = object;
+    DWSlimCell *cell	= base;
+    
+    if(!cell) 
+        cell = [[[DWSlimCell alloc] initWithStyle:UITableViewCellStyleDefault 
+                                  reuseIdentifier:identifier] autorelease];
+    
+    
+    cell.boldText   = user.firstName;
+    cell.plainText  = user.byline;
+    
+    [cell setImage:user.smallImage];
+    
+    [cell reset];
+    [cell redisplay];
+    
+    return cell;
+}
+
+//----------------------------------------------------------------------------------------------------
++ (CGFloat)heightForObject:(id)object 
+     withPresentationStyle:(NSInteger)style {
+    
+    return kCellHeight;
+}
+
+//----------------------------------------------------------------------------------------------------
++ (void)updatePresentationForCell:(id)base
+                         ofObject:(id)object
+            withPresentationStyle:(NSInteger)style
+                  withNewResource:(id)resource
+                 havingResourceID:(NSInteger)resourceID
+                           ofType:(NSInteger)resourceType {
+    
+    DWUser *user = object;
+    
+    if(resourceType == kResourceTypeSmallUserImage && user.databaseID == resourceID) {
+        
+        DWSlimCell *cell = base;
+        
+        [cell setImage:(UIImage*)resource];
+        [cell redisplay];
+    }
+}
+
+//----------------------------------------------------------------------------------------------------
++ (void)cellClickedForObject:(id)object
+                withDelegate:(id)delegate {
+    
+    SEL sel = @selector(userSelected:);
+    
+    if(![delegate respondsToSelector:sel])
+        return;
+    
+    
+    DWUser *user = object;
+    
+    [delegate performSelector:sel
+                   withObject:user];
+}
+
+
+@end
