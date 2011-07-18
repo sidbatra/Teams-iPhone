2b9eb2a0bc1b1230dd3dc10c6472bbae73d5df91
Siddharth Batra <siddharthabatra@gmail.com>
2011-07-17 23:45:38 -0700

    users view controller created

---
+++ Teams/Classes/TableViewControllers/Users/DWUsersViewController.h
@@ -0,0 +1,32 @@
+//
+//  DWUsersViewController.h
+//  Copyright 2011 Denwen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "DWTableViewController.h"
+
+@class DWUsersLogicController;
+@protocol DWUsersLogicControllerDelegate;
+
+/**
+ * Base class for table views with a list of users
+ */
+@interface DWUsersViewController : DWTableViewController {
+    DWUsersLogicController       *_usersLogicController;
+}
+
+/**
+ * Users view controller encapsulates the common display and interaction 
+ * functionality needed to display one or more users
+ */
+@property (nonatomic,retain) DWUsersLogicController *usersLogicController;
+
+
+/**
+ * Set a users logic controller delegate
+ */
+- (void)setUsersDelegate:(id<DWUsersLogicControllerDelegate>)delegate;
+
+@end
---
+++ Teams/Classes/TableViewControllers/Users/DWUsersViewController.m
@@ -0,0 +1,57 @@
+//
+//  DWUsersViewController.m
+//  Copyright 2011 Denwen. All rights reserved.
+//
+
+#import "DWUsersViewController.h"
+#import "DWUsersLogicController.h"
+#import "DWUser.h"
+#import "NSObject+Helpers.h"
+
+
+
+//----------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+@implementation DWUsersViewController
+
+@synthesize usersLogicController = _usersLogicController;
+
+//----------------------------------------------------------------------------------------------------
+- (id)init {
+    self = [super init];
+    
+    if(self) {        
+        self.usersLogicController    = [[[DWUsersLogicController alloc] init] autorelease];
+        self.usersLogicController.tableViewController = self;
+    }
+    
+    return self;
+}
+
+//----------------------------------------------------------------------------------------------------
+- (void)dealloc {    
+    self.usersLogicController        = nil;
+    
+    [super dealloc];
+}
+
+//----------------------------------------------------------------------------------------------------
+- (void)setUsersDelegate:(id<DWUsersLogicControllerDelegate>)delegate {
+    self.usersLogicController.delegate = delegate;
+}
+
+//----------------------------------------------------------------------------------------------------
+- (id)getDelegateForClassName:(NSString *)className {
+    
+    id delegate = nil;
+    
+    if([className isEqualToString:[[DWUser class] className]])
+        delegate = self.usersLogicController;
+    else
+        delegate = [super getDelegateForClassName:className];
+    
+    return delegate;
+}
+
+@end
