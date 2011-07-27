82b500420205b2e9f26b1a8edb7e2f00f8ac4598
Siddharth Batra <siddharthabatra@gmail.com>
2011-07-26 18:05:38 -0700

    creating shell for dwinteraction and an interactions array in analytics manager

--- Teams/Classes/Managers/DWAnalyticsManager.m
+++ Teams/Classes/Managers/DWAnalyticsManager.m
@@ -14,6 +14,26 @@
 //----------------------------------------------------------------------------------------------------
 @implementation DWAnalyticsManager
 
+@synthesize interactions    = _interactions;
+
 SYNTHESIZE_SINGLETON_FOR_CLASS(DWAnalyticsManager);
+
+//----------------------------------------------------------------------------------------------------
+- (id)init {
+	self = [super init];
+	
+	if(self) {
+        self.interactions  = [NSMutableArray array];
+	}
+	
+	return self;
+}
+
+//----------------------------------------------------------------------------------------------------
+- (void)dealloc {
+	self.interactions    = nil;
+    
+	[super dealloc];
+}
 
 @end
