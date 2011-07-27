94632dbb12f352bb94ebca7756e506c0d1090a95
Siddharth Batra <siddharthabatra@gmail.com>
2011-07-26 17:45:41 -0700

    creating shell for analytics manager

---
+++ Teams/Classes/Managers/DWAnalyticsManager.h
@@ -0,0 +1,18 @@
+//
+//  DWAnalyticsManager.h
+//  Copyright 2011 Denwen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface DWAnalyticsManager : NSObject {
+    
+}
+
+/**
+ * Shared sole instance of the class
+ */
++ (DWAnalyticsManager *)sharedDWAnalyticsManager;
+
+@end
---
+++ Teams/Classes/Managers/DWAnalyticsManager.m
@@ -0,0 +1,19 @@
+//
+//  DWAnalyticsManager.m
+//  Copyright 2011 Denwen. All rights reserved.
+//
+
+#import "DWAnalyticsManager.h"
+
+#import "SynthesizeSingleton.h"
+
+
+
+//----------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+@implementation DWAnalyticsManager
+
+SYNTHESIZE_SINGLETON_FOR_CLASS(DWAnalyticsManager);
+
+@end
