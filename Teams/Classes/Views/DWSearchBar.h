49e3690ad9228d02033ac55a8dce44e034d0dfa3
Siddharth Batra <siddharthabatra@gmail.com>
2011-07-30 14:33:53 -0700

    organizing views into their own folder. DWSearchBar shell added

---
+++ Teams/Classes/Views/DWSearchBar.h
@@ -0,0 +1,16 @@
+//
+//  DWSearchBar.h
+//  Teams
+//
+//  Created by Siddharth Batra on 7/30/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface DWSearchBar : UIView {
+    
+}
+
+@end
---
+++ Teams/Classes/Views/DWSearchBar.m
@@ -0,0 +1,37 @@
+//
+//  DWSearchBar.m
+//  Teams
+//
+//  Created by Siddharth Batra on 7/30/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "DWSearchBar.h"
+
+
+@implementation DWSearchBar
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
