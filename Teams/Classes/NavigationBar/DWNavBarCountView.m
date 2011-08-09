8eee3071135501e9ac43d9de57f74c9a6f87b793
Deepak Rao <raodpk@gmail.com>
2011-08-08 17:44:18 -0700

    login return key corrected and notifications color fixed

--- Teams/Classes/NavigationBar/DWNavBarCountView.m
+++ Teams/Classes/NavigationBar/DWNavBarCountView.m
@@ -93,11 +93,13 @@ static CGFloat   const kDisabledOpacity     = 0.5;
     
     if (count) {
         countLabel.textColor        = [UIColor whiteColor];        
-        countLabel.backgroundColor  = [UIColor colorWithRed:0.8 
-                                                      green:0.0 
-                                                       blue:0.0 
-                                                      alpha:1.0];
+        countLabel.backgroundColor  = [UIColor colorWithRed:0.8 green:0.0 blue:0.0 alpha:1.0];
         countLabel.alpha            = 1.0;
+    }
+    else {
+        countLabel.textColor        = [UIColor colorWithRed:0.2 green:0.2 blue:0.2 alpha:1.0];        
+        countLabel.backgroundColor  = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0.5];
+        countLabel.alpha            = kDisabledOpacity;
     }
 }
 
