0dea81bdc8106c82ebb00ecc3b7e07c33937d84f
Siddharth Batra <siddharthabatra@gmail.com>
2011-08-10 17:18:25 -0700

    touch model beefed up to handle json data

--- Teams/Classes/Models/DWTouch.h
+++ Teams/Classes/Models/DWTouch.h
@@ -7,11 +7,25 @@
 
 #import "DWPoolObject.h"
 
+@class DWUser;
+
 /**
  * Touch model reprsenting an interaction between an item and a user
  */
 @interface DWTouch : DWPoolObject {
+    DWUser          *_user;
     
+    NSTimeInterval	_createdAtTimestamp;
 }
+
+/**
+ * User object of the user who created the touch
+ */
+@property (nonatomic,retain) DWUser *user;
+
+/**
+ * Timestamp of the creation of the touch 
+ */
+@property (nonatomic,readonly) NSTimeInterval createdAtTimestamp;
 
 @end
