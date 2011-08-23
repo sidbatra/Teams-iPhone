567880cea22842baf229699260b9b7d0e13ffbe8
Siddharth Batra <siddharthabatra@gmail.com>
2011-08-23 04:44:57 -0700

    signature update

--- Teams/main.m
+++ Teams/main.m
@@ -1,9 +1,6 @@
 //
 //  main.m
-//  Denwen
-//
-//  Created by Deepak Rao on 1/19/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Denwen. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
